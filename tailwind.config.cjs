99d0bef88554264cbcb1f00366ea5ffa5eec4ee0
Carlos Barbosa <socarlosb@gmail.com>
2022-12-22 13:24:56 +0000

    add theme options

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,19 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "390",
+      desktop: "1024px",
+    },
+    colors: {
+      indigo: "#4f46e5",
+      "dark-indigo": "#312e81",
+      amber: "#fbbf24",
+      "dark-amber": "#92400e",
+      light: "#f5f5f5",
+      dark: "#171717",
+      transparent: "transparent",
+    },
   },
   plugins: [],
 };
